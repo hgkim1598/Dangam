56cdf17f41a4423ed06b2e0e054db1dc97d43596
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-02-13 18:13:02 +0900

    2024-02-13 HWY

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import FourIdioms from '@/components/FourIdioms'
 
 Vue.use(Router)
@@ -8,14 +7,9 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/home',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
       path: '/',
       name: 'FourIdioms',
       component: FourIdioms
-    }
+    },
   ]
 })
