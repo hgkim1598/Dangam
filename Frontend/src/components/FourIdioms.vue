a1081769cb7d818ecdee18e529274830c7b15c16
hgkim1598 <hgkim1598@gmail.com>
2024-02-16 16:10:20 +0900

    Resolve merge conflict

--- Frontend/src/components/FourIdioms.vue
+++ Frontend/src/components/FourIdioms.vue
@@ -9,19 +9,7 @@
         <b-icon icon="search"></b-icon>
       </b-button>
     </div>
-    <!-- 게시판 데이터 표시 -->
-    <div>
-      <b-table striped hover :items="items" :fields="fields">
-      </b-table>
-    </div>
-    <!-- 페이지 번호 표시 및 변경 -->
-    <div class="d-flex justify-content-end">
-      <div>
-        <b-button @click="changePage(pageNumber - 1)" :disabled="pageNumber <= 1">이전 페이지</b-button>
-        <span>{{ pageNumber }} / {{ totalPage }}</span>
-        <b-button @click="changePage(pageNumber + 1)" :disabled="pageNumber >= totalPage">다음 페이지</b-button>
-     </div>
-   </div>
+    
   <div class="table-container">
     <b-button @click="showModal" class="nr_button">신규 등록</b-button>
     <b-dropdown v-if="categories.length > 0" ref="categoryDropdown" class="category-dropdown" variant="primary">
@@ -50,6 +38,7 @@
           <b-button size="sm" variant="warning" @click="editItem(row.item)" class="control-button">수정</b-button>
           <b-button size="sm" variant="danger" @click="deleteItem(row.item)" class="control-button last-button">삭제</b-button>
         </div>
+        
       </template>
 
       <!-- 커스텀 컬럼 정의 -->
@@ -72,6 +61,16 @@
       </template>
     </b-table>
   </div>
+  <div>
+          <!-- 페이지 번호 표시 및 변경 -->
+    <div class="d-flex justify-content-end">
+      <div>
+        <b-button @click="changePage(pageNumber - 1)" :disabled="pageNumber <= 1">이전 페이지</b-button>
+        <span>{{ pageNumber }} / {{ totalPage }}</span>
+        <b-button @click="changePage(pageNumber + 1)" :disabled="pageNumber >= totalPage">다음 페이지</b-button>
+     </div>
+   </div>
+        </div>
 </div>
 </template>
 
@@ -117,10 +116,32 @@ export default {
     }
   },
   methods: {
-    async fetchData() {
+    fetchData1(page, keyword) {
+      page = Number(page);
+
+      let apiUrl = `http://192.168.0.149:8000/fourchar`;
+      if (keyword) {
+        apiUrl += `/filter/?keyword=${keyword}`;
+      } else {
+        apiUrl += `?p=${page}`;
+      }
+
+      axios.get(apiUrl)
+        .then(response => {
+          this.totalPage = response.data.total_page;
+          this.items = response.data.content;
+        })
+        .catch(error => {
+          console.error('데이터를 불러오는 중 오류 발생:', error);
+        });
+    },
+    async fetchData(page) {
       try {
-        const apiUrl = 'http://192.168.0.149:8000/fourchar';
-        const response = await axios.get(apiUrl);
+        page = 1;
+        let apiUrl = 'http://192.168.0.149:8000/fourchar';
+        const apiUrl1 = apiUrl += `?p=${page}`;
+        const response = await axios.get(apiUrl1);
+        this.totalPage = response.data.total_page;
         this.items = response.data.content;
         // 각 아이템에 detailsShowing 프로퍼티 추가
         this.items.forEach(item => {
@@ -219,36 +240,19 @@ export default {
       // 선택된 카테고리를 이전에 선택된 카테고리로 저장합니다.
       this.prevSelectedCategories = [...this.selectedCategories];
     },
-    fetchData(page, keyword) {
-      page = Number(page);
 
-      let apiUrl = `http://192.168.0.149:8000/fourchar`;
-      if (keyword) {
-        apiUrl += `/filter/?keyword=${keyword}`;
-      } else {
-        apiUrl += `?p=${page}`;
-      }
 
-      axios.get(apiUrl)
-        .then(response => {
-          this.totalPage = response.data.total_page;
-          this.items = response.data.content;
-        })
-        .catch(error => {
-          console.error('데이터를 불러오는 중 오류 발생:', error);
-        });
-    },
     changePage(page) {
       if (page > 0 && page <= this.totalPage) {
         this.pageNumber = page;
-        this.fetchData(page, this.searchKeyword); // 페이지 변경 시 검색어도 함께 전달
+        this.fetchData1(page, this.searchKeyword); // 페이지 변경 시 검색어도 함께 전달
       }
     },
     search() {
       const trimmedKeyword = this.searchKeyword.trim();
       if (trimmedKeyword !== '') {
         this.pageNumber = 1; // 검색할 때 페이지 번호를 1로 초기화합니다.
-        this.fetchData(this.pageNumber, trimmedKeyword); // 검색 시 검색어도 함께 전달
+        this.fetchData1(this.pageNumber, trimmedKeyword); // 검색 시 검색어도 함께 전달
       }
     },
   }
