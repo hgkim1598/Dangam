4c2660640b4644ebbadfdab74b285f85a2fa7514
hgkim1598 <hgkim1598@gmail.com>
2024-02-19 14:30:42 +0900

    FEAT 카테고리 필터링 페이지네이션 구현 완료

--- Frontend/src/components/FourIdioms.vue
+++ Frontend/src/components/FourIdioms.vue
@@ -10,68 +10,68 @@
       </b-button>
     </div>
     
-  <div class="table-container">
-    <b-button @click="showModal" class="nr_button">신규 등록</b-button>
-    <b-dropdown v-if="categories.length > 0" ref="categoryDropdown" class="category-dropdown" variant="primary">
-      <template #button-content>
-        카테고리 선택
-      </template>
-      <b-form-group  class="category-dropdown-list">
-        <b-form-checkbox-group v-model="selectedCategories">
-          <b-form-checkbox v-for="(cat, index) in categories" :key="index" :value="cat">{{ cat }}</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-      <b-button @click="closeDropdown">취소</b-button>
-      <b-button @click="submitSelectedCategories" variant="success">확인</b-button>
-    </b-dropdown>
-    <b-modal v-model="modalVisible" :title="modalTitle" hide-footer>
-      <!-- Create.vue 컴포넌트 렌더링 -->
-      <Create :isEditMode="isEditMode" :editId="editId" @closeModal="closeModal" />
-    </b-modal>
-    <b-table striped hover :items="items" :fields="fields">
-      <template #cell(actions)="row">
-        <!-- 제어 버튼 -->
-        <div class="btn-group" role="group">
-          <b-button size="sm" @click="toggleDetails(row.item)" class="control-button">
-            {{ row.item.detailsShowing ? '숨김' : '펼침' }}
-          </b-button>
-          <b-button size="sm" variant="warning" @click="editItem(row.item)" class="control-button">수정</b-button>
-          <b-button size="sm" variant="danger" @click="deleteItem(row.item)" class="control-button last-button">삭제</b-button>
-        </div>
-        
-      </template>
+    <div class="table-container">
+      <b-button @click="showModal" class="nr_button">신규 등록</b-button>
+      <b-dropdown v-if="categories.length > 0" ref="categoryDropdown" class="category-dropdown" variant="primary">
+        <template #button-content>
+          카테고리 선택
+        </template>
+        <b-form-group  class="category-dropdown-list">
+          <b-form-checkbox-group v-model="selectedCategories">
+            <b-form-checkbox v-for="(cat, index) in categories" :key="index" :value="cat">{{ cat }}</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button @click="closeDropdown">취소</b-button>
+        <b-button @click="submitSelectedCategories" variant="success">확인</b-button>
+      </b-dropdown>
+      <b-modal v-model="modalVisible" :title="modalTitle" hide-footer>
+        <!-- Create.vue 컴포넌트 렌더링 -->
+        <Create :isEditMode="isEditMode" :editId="editId" @closeModal="closeModal" />
+      </b-modal>
+      <b-table striped hover :items="items" :fields="fields">
+        <template #cell(actions)="row">
+          <!-- 제어 버튼 -->
+          <div class="btn-group" role="group">
+            <b-button size="sm" @click="toggleDetails(row.item)" class="control-button">
+              {{ row.item.detailsShowing ? '숨김' : '펼침' }}
+            </b-button>
+            <b-button size="sm" variant="warning" @click="editItem(row.item)" class="control-button">수정</b-button>
+            <b-button size="sm" variant="danger" @click="deleteItem(row.item)" class="control-button last-button">삭제</b-button>
+          </div>
+          
+        </template>
 
-      <!-- 커스텀 컬럼 정의 -->
-      <template #cell(contents_detail)="data">
-        <div>
-          <b-card v-if="data.item.detailsShowing">
-            <p>{{ data.item.contents_detail }}</p>
+        <!-- 커스텀 컬럼 정의 -->
+        <template #cell(contents_detail)="data">
+          <div>
+            <b-card v-if="data.item.detailsShowing">
+              <p>{{ data.item.contents_detail }}</p>
+            </b-card>
+            <span v-else>{{ truncateText(data.item.contents_detail, 50) }}</span>
+          </div>
+        </template>
+
+        <!-- 확장된 세부 정보 -->
+        <template #row-details="row">
+          <b-card v-if="row.item.detailsShowing">
+            <ul>
+              <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
+            </ul>
           </b-card>
-          <span v-else>{{ truncateText(data.item.contents_detail, 50) }}</span>
-        </div>
-      </template>
-
-      <!-- 확장된 세부 정보 -->
-      <template #row-details="row">
-        <b-card v-if="row.item.detailsShowing">
-          <ul>
-            <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
-          </ul>
-        </b-card>
-      </template>
-    </b-table>
-  </div>
-  <div>
-          <!-- 페이지 번호 표시 및 변경 -->
-    <div class="d-flex justify-content-end">
-      <div>
-        <b-button @click="changePage(pageNumber - 1)" :disabled="pageNumber <= 1">이전 페이지</b-button>
-        <span>{{ pageNumber }} / {{ totalPage }}</span>
-        <b-button @click="changePage(pageNumber + 1)" :disabled="pageNumber >= totalPage">다음 페이지</b-button>
+        </template>
+      </b-table>
+    </div>
+    <div>
+      <!-- 페이지 번호 표시 및 변경 -->
+      <div class="d-flex justify-content-end">
+        <div>
+          <b-button @click="changePage(pageNumber - 1)" :disabled="pageNumber <= 1">이전 페이지</b-button>
+          <span>{{ pageNumber }} / {{ totalPage }}</span>
+          <b-button @click="changePage(pageNumber + 1)" :disabled="pageNumber >= totalPage">다음 페이지</b-button>
+       </div>
      </div>
    </div>
-        </div>
-</div>
+ </div>
 </template>
 
 <script>
@@ -116,37 +116,28 @@ export default {
     }
   },
   methods: {
-    fetchData1(page, keyword) {
-      page = Number(page);
-
-      let apiUrl = `http://192.168.0.149:8000/fourchar`;
-      if (keyword) {
-        apiUrl += `/filter/?keyword=${keyword}`;
-      } else {
-        apiUrl += `?p=${page}`;
-      }
-
-      axios.get(apiUrl)
-        .then(response => {
-          this.totalPage = response.data.total_page;
-          this.items = response.data.content;
-        })
-        .catch(error => {
-          console.error('데이터를 불러오는 중 오류 발생:', error);
-        });
-    },
-    async fetchData(page) {
+    fetchData(page, keyword) {
       try {
-        page = 1;
-        let apiUrl = 'http://192.168.0.149:8000/fourchar';
-        const apiUrl1 = apiUrl += `?p=${page}`;
-        const response = await axios.get(apiUrl1);
-        this.totalPage = response.data.total_page;
-        this.items = response.data.content;
-        // 각 아이템에 detailsShowing 프로퍼티 추가
-        this.items.forEach(item => {
-          this.$set(item, 'detailsShowing', false);
-        });
+        page = page || 1;
+        let apiUrl = `http://192.168.0.149:8000/fourchar`;
+        if (keyword) {
+          apiUrl += `/filter/?keyword=${keyword}`;
+        } else {
+          apiUrl += `?p=${page}`;
+        }
+
+        axios.get(apiUrl)
+          .then(response => {
+            this.totalPage = response.data.total_page;
+            this.items = response.data.content;
+            // 각 아이템에 detailsShowing 프로퍼티 추가
+            this.items.forEach(item => {
+              this.$set(item, 'detailsShowing', false);
+            });
+          })
+          .catch(error => {
+            console.error('데이터를 불러오는 중 오류 발생:', error);
+          });
       } catch (error) {
         console.error('데이터를 불러오는 중 오류 발생:', error);
       }
@@ -219,21 +210,22 @@ export default {
       this.selectedCategories = this.prevSelectedCategories;
     },
     submitSelectedCategories() {
-  // 선택된 카테고리들을 백엔드에서 요구하는 형식에 맞게 가공합니다.
-  const categoriesParams = this.selectedCategories.map(category => `categories=${encodeURIComponent(category)}`).join('&');
+      // 선택된 카테고리들을 백엔드에서 요구하는 형식에 맞게 가공합니다.
+      const categoriesParams = this.selectedCategories.map(category => `categories=${encodeURIComponent(category)}`).join('&');
 
-  axios.get(`http://192.168.0.149:8000/fourchar/filter/?${categoriesParams}`)
-    .then(response => {
-      console.log(response.data);
-      this.items = response.data.content; // 받아온 데이터를 items에 할당
-      // 드롭다운 박스를 닫습니다.
-      this.$refs.categoryDropdown.hide();
-    })
-    .catch(error => {
-      console.error('GET 요청 중 오류가 발생했습니다.', error);
-      // 오류 발생 시 처리할 내용을 추가할 수 있습니다.
-    });
-},
+      axios.get(`http://192.168.0.149:8000/fourchar/filter/?${categoriesParams}&p=${this.pageNumber}`)
+        .then(response => {
+          console.log(response.data);
+          this.totalPage = response.data.total_page;
+          this.items = response.data.content; // 받아온 데이터를 items에 할당
+          // 드롭다운 박스를 닫습니다.
+          this.$refs.categoryDropdown.hide();
+        })
+        .catch(error => {
+          console.error('GET 요청 중 오류가 발생했습니다.', error);
+          // 오류 발생 시 처리할 내용을 추가할 수 있습니다.
+        });
+    },
     toggleDropdown() {
       // 드롭다운 박스를 열거나 닫습니다.
       this.$refs.categoryDropdown.toggle();
@@ -243,16 +235,23 @@ export default {
 
 
     changePage(page) {
-      if (page > 0 && page <= this.totalPage) {
-        this.pageNumber = page;
-        this.fetchData1(page, this.searchKeyword); // 페이지 변경 시 검색어도 함께 전달
-      }
-    },
+  if (page > 0 && page <= this.totalPage) {
+    // 카테고리가 선택되어 있으면 선택된 카테고리로 데이터를 가져옵니다.
+    if (this.selectedCategories.length > 0) {
+      this.pageNumber = page;
+      this.submitSelectedCategories();
+    } else {
+      // 카테고리가 선택되어 있지 않으면 기존의 fetchData 메소드를 사용하여 데이터를 가져옵니다.
+      this.fetchData(page, this.searchKeyword);
+    }
+  }
+},
+
     search() {
       const trimmedKeyword = this.searchKeyword.trim();
       if (trimmedKeyword !== '') {
         this.pageNumber = 1; // 검색할 때 페이지 번호를 1로 초기화합니다.
-        this.fetchData1(this.pageNumber, trimmedKeyword); // 검색 시 검색어도 함께 전달
+        this.fetchData(1, trimmedKeyword); // 검색 시 검색어도 함께 전달
       }
     },
   }
