5963de1cd2601fdf2c84a41c01e96507e38a965a
HWY <hjy0859@gmail.com>
2024-03-08 15:29:10 +0900

    이미지 크롤링 1차 완료

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import FourIdioms from '@/components/FourIdioms'
 import WiseSaying from '@/components/WiseSaying'
-import ImageCrowling from '@/components/ImageCrowling'
+import ImageCrawling from '@/components/ImageCrawling'
 
 Vue.use(Router)
 
@@ -25,9 +25,9 @@ export default new Router({
       component: WiseSaying
     },
     {
-      path: '/ImageCrowling',
-      name: 'ImageCrowling', // 홈 페이지로 사용할 컴포넌트를 여기에 설정할 수 있습니다.
-      component: ImageCrowling // 만약 루트 경로가 별도의 홈 페이지로 사용된다면 해당 컴포넌트로 설정합니다.
+      path: '/ImageCrawling',
+      name: 'ImageCrawling', // 홈 페이지로 사용할 컴포넌트를 여기에 설정할 수 있습니다.
+      component: ImageCrawling // 만약 루트 경로가 별도의 홈 페이지로 사용된다면 해당 컴포넌트로 설정합니다.
     },
     
   ]
